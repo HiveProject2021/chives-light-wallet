1bd5c2baaeff460b5ea1d13864256896f3162d90
HiveProject <jwang123@nyit.edu>
2021-12-16 21:16:17 -0800

    Update cat_constants.py

--- chives/wallet/cc_wallet/cat_constants.py
+++ chives/wallet/cc_wallet/cat_constants.py
@@ -1,16 +1,30 @@
-SPACEBUCKS = {
-    "asset_id": "78ad32a8c9ea70f27d73e9306fc467bab2a6b15b30289791e37ab6e8612212b1",
-    "name": "Spacebucks",
-    "symbol": "SBX",
+KITTY = {
+    "asset_id": "3e3a7614a02d9714a21927ef99c7ef9bf8270e374dc6ecc48f2619cbc70c4ddc",
+    "name": "KITTY",
+    "symbol": "KTY",
 }
 
-MARMOT = {
-    "asset_id": "8ebf855de6eb146db5602f0456d2f0cbe750d57f821b6f91a8592ee9f1d4cf31",
-    "name": "Marmot",
-    "symbol": "MRMT",
+CHIVESPET = {
+    "asset_id": "ea443ca6390d227cdc838acf045cd077accefa4bf7f96b3d4b58c24461fe9c97",
+    "name": "CHIVESPET",
+    "symbol": "CHVP",
+}
+
+PONY = {
+    "asset_id": "44bdcb770528c5e0d2fd95afddb029c7c0424bd4875b9bc962cc36c84194afba",
+    "name": "PONY",
+    "symbol": "PONY",
+}
+
+USDV = {
+    "asset_id": "a43cd19274383d988f6c204c317f31ecd9c7a404e086e7ccefa0bcbdeb9e42ca",
+    "name": "USDV",
+    "symbol": "USDV",
 }
 
 DEFAULT_CATS = {
-    SPACEBUCKS["asset_id"]: SPACEBUCKS,
-    MARMOT["asset_id"]: MARMOT,
+    KITTY["asset_id"]: KITTY,
+    CHIVESPET["asset_id"]: CHIVESPET,
+    PONY["asset_id"]: PONY,
+    USDV["asset_id"]: USDV,
 }
